74b4fb104facfd3469b4aacbca29b7c2917fe765
SouleymaneSy7 <souleymanesyservices@gmail.com>
2025-02-01 20:34:46 +0000

    fix: fix the page errors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,10 +5,10 @@ import Main from "@/components/Main";
 
 const Page: React.FC = () => {
   return (
-    <React.Fragment>
+    <div>
       <Header />
       <Main />
-    </React.Fragment>
+    </div>
   );
 };
 
