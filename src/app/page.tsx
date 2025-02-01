7142785a4a04cb92d36b5dd59f1ebe8fd66ad054
SouleymaneSy7 <souleymanesyservices@gmail.com>
2025-02-01 15:16:25 +0000

    feat: update the homepage component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 
 import Header from "@/components/Header";
+import Main from "@/components/Main";
 
 const Home: React.FC = () => {
   return (
     <React.Fragment>
       <Header />
+      <Main />
     </React.Fragment>
   );
 };
