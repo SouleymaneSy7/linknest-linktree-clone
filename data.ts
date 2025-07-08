ecc188216fe840bfddc7b9f7b9461df1c151b374
SouleymaneSy7 <souleymanesyservices@gmail.com>
2025-07-08 15:17:47 +0000

    chore(data): update the user contents data

--- data.ts
+++ data.ts
@@ -1,13 +1,15 @@
 import profileImg from "./public/images/profile-img.jpg";
-import {
-  FacebookIcon,
-  LinkedInIcon,
-  MessengerIcon,
-  PhoneIcon,
-  SnapchatIcon,
-  TwitterIcon,
-  WhatsappIcon,
-} from "@/components/Icons/Icons.component";
+// import {
+//   FacebookIcon,
+//   LinkedInIcon,
+//   MessengerIcon,
+//   PhoneIcon,
+//   SnapchatIcon,
+//   TwitterIcon,
+//   WhatsappIcon,
+// } from "@/components/Icons/Icons.component";
+
+import { PhoneIcon, GithubIcon, FacebookIcon, LinkedinIcon, TwitterIcon, CodepenIcon, SlackIcon, DribbbleIcon } from 'lucide-react'
 
 const profileData = {
   profile_name: "Mohamed Lamine Sy",
@@ -16,14 +18,14 @@ const profileData = {
   profile_bio: "Le meilleur chauffeur que je connaisse!",
   profile_links: [
     {
-      name: "Curriculum Vitæ - (CV)",
-      title: "Consulter Mon CV",
+      name: "Portfolio",
+      title: "Consulter Mon Portfolio",
       url: "https://www.google.com",
     },
     {
       name: "E-mail",
       title: "Envoyez-moi Un E-mail",
-      url: "mailto:souleymanesyservices@gmail.com",
+      url: "mailto:souleymanesydeveloppers@gmail.com",
     },
   ],
   profile_socials: [
@@ -33,34 +35,41 @@ const profileData = {
       src: PhoneIcon,
     },
     {
+     title: "Consulter mon Github",
+     url: "https://github.com/SouleymaneSy7",
+     src: GithubIcon,
+    },
+    {
       title: "Contacter moi sur Facebook",
       url: "https://www.facebook.com",
       src: FacebookIcon,
     },
-    {
-      title: "Contacter moi sur Whatsapp",
-      url: "https://www.whatsapp.com",
-      src: WhatsappIcon,
+{
+      title: "Contacter moi sur LinkedIn",
+      url: "https://www.linkedin.com",
+      src: LinkedinIcon,
     },
+
     {
       title: "Contacter moi sur Twitter",
       url: "https://www.twitter.com",
       src: TwitterIcon,
     },
     {
-      title: "Contacter moi sur Messenger",
-      url: "https://www.messenger.com",
-      src: MessengerIcon,
+      title: "Contacter moi sur Dribbble",
+      url: "https://www.dribbble.com",
+      src: DribbbleIcon,
     },
-    {
-      title: "Contacter moi sur LinkedIn",
-      url: "https://www.linkedin.com",
-      src: LinkedInIcon,
+        {
+      title: "Contacter moi sur Slack",
+      url: "https://www.slack.com",
+      src: SlackIcon,
     },
+
     {
-      title: "Contacter moi sur Snapchat",
-      url: "https://www.snapchat.com",
-      src: SnapchatIcon,
+      title: "Contacter moi sur Codepen",
+      url: "https://www.codepen.com",
+      src: CodepenIcon,
     },
   ],
 };
