3923e337705f6c9b3675b95da9b49f87acab2eb0
SouleymaneSy7 <souleymanesyservices@gmail.com>
2025-07-08 14:51:26 +0000

    feat: add the list component

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,10 +1,10 @@
-import * as React from "react";
+import * as React from 'react'
 
 type ListPropsType<Item, As extends React.ElementType> = {
-  items: Item[];
-  renderItem: (item: Item) => React.ReactNode;
-  as?: As;
-};
+  items: Item[]
+  renderItem: (item: Item) => React.ReactNode
+  as?: As
+}
 
 function List<Item, As extends React.ElementType>({
   items,
@@ -13,15 +13,13 @@ function List<Item, As extends React.ElementType>({
   ...delegatedProps
 }: ListPropsType<Item, As> &
   Omit<React.ComponentPropsWithoutRef<As>, keyof ListPropsType<Item, As>>) {
-  const Component = as ?? "ul";
+  const Component = as ?? 'ul'
 
   return (
     <Component {...delegatedProps} role="list">
       {items.map(renderItem)}
     </Component>
-  );
+  )
 }
 
-export default List;
-
-
+export default List
