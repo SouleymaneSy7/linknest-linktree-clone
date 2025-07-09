3481090bc620b47f951cfd90b04f3fa05a8f379d
SouleymaneSy7 <souleymanesyservices@gmail.com>
2025-07-09 15:35:51 +0000

    feat: update the header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,26 +1,29 @@
-'use client'
+"use client";
 
-import React from 'react'
+import * as React from "react";
 
-import SwithCustomization from '@/components/ui/switch'
-import Container from '@/components/Container'
-import Link from 'next/link'
+import SwithCustomization from "@/components/ui/switch";
+import Container from "@/components/Container";
+import Link from "next/link";
 
 const Header: React.FC = () => {
   return (
-    <Container as={'header'} className="container pt-8">
+    <Container as={"header"} className="container pt-8">
       <nav
         role="navigation"
         className="flex justify-between items-center w-full"
       >
-        <Link href={'/'} className="font-space-grotesk font-fw-bold text-fs-lg">
+        <Link
+          href={"/"}
+          className="font-space-grotesk font-bold text-sub-heading"
+        >
           Link<span className="text-primary">Nest</span>
         </Link>
 
         <SwithCustomization />
       </nav>
     </Container>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
