2c0899ff3c39e109cf90f033c9147252f9c933e2
SouleymaneSy7 <souleymanesyservices@gmail.com>
2025-07-09 15:35:29 +0000

    feat: update the font-family in the root-layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,43 +1,43 @@
-import type { Metadata } from 'next'
-import { Space_Grotesk, AR_One_Sans } from 'next/font/google'
-import { ThemeProvider } from '@/components/ThemeProvider'
-import './globals.css'
+import type { Metadata } from "next";
+import { Space_Grotesk, AR_One_Sans } from "next/font/google";
+import { ThemeProvider } from "@/components/ThemeProvider";
+import "./globals.css";
 
 const spaceGrotesk = Space_Grotesk({
-  variable: '--font-space-grotesk',
-  subsets: ['latin'],
-  weight: '700',
-  display: 'swap',
+  variable: "--font-space-grotesk",
+  subsets: ["latin"],
+  weight: "700",
+  display: "swap",
   adjustFontFallback: false,
-})
+});
 
 const AROneSans = AR_One_Sans({
-  variable: '--font-ar-one-sans',
-  subsets: ['latin'],
-  weight: ['400', '700'],
-  display: 'swap',
+  variable: "--font-ar-one-sans",
+  subsets: ["latin"],
+  weight: ["400", "700"],
+  display: "swap",
   adjustFontFallback: false,
-})
+});
 
 export const metadata: Metadata = {
-  title: 'Linknest - Linktree Clone',
-  description: 'A Linktree Clone Project For Next.js Practice',
-}
+  title: "Linknest - Linktree Clone",
+  description: "A Linktree Clone Project For Next.js Practice",
+};
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
       <body
-        className={`${AROneSans.className} ${spaceGrotesk.className} antialiased`}
+        className={`${AROneSans.variable} ${spaceGrotesk.variable} antialiased`}
       >
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
           {children}
         </ThemeProvider>
       </body>
     </html>
-  )
+  );
 }
