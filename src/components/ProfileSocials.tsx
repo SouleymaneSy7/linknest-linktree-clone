3907d4d383a54603b1e5d2f7158fe6c66d31d719
SouleymaneSy7 <souleymanesyservices@gmail.com>
2025-07-10 17:16:47 +0000

    feat: update the profile socials components

--- src/components/ProfileSocials.tsx
+++ src/components/ProfileSocials.tsx
@@ -1,4 +1,10 @@
 import * as React from 'react'
+import { Button } from '@/components/ui/button'
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+} from '@/components/ui/tooltip'
 
 import Container from './Container'
 import List from './List'
@@ -15,25 +21,34 @@ const ProfileSocials: React.FC<ProfileSocialsPropsType> = ({ socialsList }) => {
   return (
     <Container as="div" className="mt-12 grid place-items-center">
       <List
-        className="flex flex-wrap items-center justify-center gap-2 lg:gap-4"
+        className="flex items-center gap-4"
         items={socialsList}
         renderItem={({ title, url, src }) => {
           const Icon = src
 
           return (
             <li key={title}>
-              <a
-                href={url}
-                target="_blank"
-                title={title}
-                className="text-foreground hover:text-primary transition-colors"
-              >
-                <Icon
-                  className="text-current-color h-6 w-6 lg:h-8 lg:w-8"
-                  stroke="currentcolor"
-                  strokeWidth={2}
-                />
-              </a>
+              <Tooltip>
+                <TooltipTrigger asChild>
+                  <Button variant="ghost" asChild>
+                    <a
+                      href={url}
+                      target="_blank"
+                      className="text-foreground hover:text-primary transition-colors"
+                    >
+                      <Icon
+                        className="text-current-color w- lg:h-8 lg:w-8"
+                        stroke="currentcolor"
+                        strokeWidth={2}
+                      />
+                    </a>
+                  </Button>
+                </TooltipTrigger>
+
+                <TooltipContent>
+                  <p>{title}</p>
+                </TooltipContent>
+              </Tooltip>
             </li>
           )
         }}
