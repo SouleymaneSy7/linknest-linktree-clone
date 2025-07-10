24b188d14a3be60966449c799f101d88b5fdb936
SouleymaneSy7 <souleymanesyservices@gmail.com>
2025-07-10 17:18:07 +0000

    feat: add the dot pattern components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,22 @@
-import React from 'react'
-
 import Header from '@/components/Header'
 import Main from '@/components/Main'
+
+import { cn } from '@/lib/utils'
+import { DotPattern } from '@/components/layouts/DotPattern'
 
 export default function Home() {
   return (
     <>
+      <div className="absolute inset-0 -z-50 flex h-[500px] w-full flex-col items-center justify-center overflow-hidden">
+        <DotPattern
+          glow={true}
+          className={cn(
+            '[mask-image:radial-gradient(300px_circle_at_center,white,transparent)]'
+          )}
+        />
+      </div>
       <Header />
+
       <Main />
     </>
   )
